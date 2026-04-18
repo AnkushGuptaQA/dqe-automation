ebc515fc3d58ae9469626da06967a1b6f47b220a
AnkushGupta <ankush_gupta@epam.com>
2026-04-18 21:51:50 +0530

    Commiting Changes to Git

--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -6,6 +6,7 @@ def test_file_not_empty(read_csv_content):
     content = read_csv_content
     assert content, "CSV file is empty!"
 
+@pytest.mark.validate_csv
 @pytest.mark.xfail(reason="Duplicate row validation is expected to fail")
 def test_duplicates(read_csv_content):
     seen = set()
@@ -14,15 +15,18 @@ def test_duplicates(read_csv_content):
         assert row_tuple not in seen, f"Duplicate row found: {row}"
         seen.add(row_tuple)
 
+@pytest.mark.validate_csv
 def test_validate_schema(validate_schema, expected_schema):
     assert validate_schema==expected_schema, f"Schema mismatch! Expected: {expected_schema}, Found: {list(actual_schema)}"
 
+@pytest.mark.validate_csv
 @pytest.mark.skip(reason="Age validation is skipped as per requirements")
 def test_age_column_valid(read_csv_content):
     for row in read_csv_content:
         age = int(row['age'])
         assert 0 <= age <= 100, f"Invalid age {age} for id {row['id']}"
 
+@pytest.mark.validate_csv
 def test_email_column_valid(read_csv_content):
     email_regex = r"[^@]+@[^@]+\.[^@]+"
     for row in read_csv_content:
